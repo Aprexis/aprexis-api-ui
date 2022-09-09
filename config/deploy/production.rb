60ff045539b57e05c7b5d8c83f33b9a6f672de77
Ian Brown <ibrown@bear-code.com>
2022-09-09 12:16:26 -0400

    Added production deploy and build.

---
+++ config/deploy/production.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+set :domain, '10.138.0.98'
+set :deploy_to, "/home/webapp/production/#{fetch(:application)}"
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+server fetch(:domain).to_s, user: 'webapp', roles: %w[web app db], primary: true
+
+namespace :deploy do
+  task :yarn_deploy do
+    on roles fetch(:yarn_roles) do
+      within fetch(:yarn_target_path, release_path) do
+        execute fetch(:yarn_bin), 'build:production'
+      end
+    end
+  end
+
+  before 'symlink:release', :yarn_deploy
+end
